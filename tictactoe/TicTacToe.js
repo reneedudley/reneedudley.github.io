67b23010206585e86758a152b2443b06c0f3519a
reneedudley <4reneedudley@gmail.com>
2015-10-20 19:17:36 -0500

    alternates

--- tictactoe/TicTacToe.js
+++ tictactoe/TicTacToe.js
@@ -1,10 +1,22 @@
-'use strict';
-
+'use strict'; //keeps from assigning global variables
 
 $(document).ready(function () {
+	//siwtch players
+	var playerturn = 'X';
 	var $datacell = $('[data-cell]');
 	$datacell.click(function(){
-  		$(this).text('X')
+  		$(this).text(playerturn);
+  		// if (playerturn ==='X'){
+  		// 	playerturn = 'O';
+  		// }
+  		// else{
+  		// 	playerturn = 'X'
+  		// }
+  		playerturn = (playerturn ==='X') ? 'O' : 'X';
   	});
 
+
+
+
 }); 
+
