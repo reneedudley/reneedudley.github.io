0aa0686c669ba2adc68377b92a7a73d1cb7fb702
reneedudley <4reneedudley@gmail.com>
2015-10-20 19:42:17 -0500

    gettin there

--- tictactoe/TicTacToe.js
+++ tictactoe/TicTacToe.js
@@ -12,10 +12,30 @@ $(document).ready(function () {
   		// else{
   		// 	playerturn = 'X'
   		// }
+  		checkForWin();
   		playerturn = (playerturn ==='X') ? 'O' : 'X';
   	});
+  	function horizontalWin() {
+    return $('[data-cell="0"]').text() === playerTurn && ('[data-cell="1"]').text() === playerTurn && ('[data-cell="2"]').text() === playerTurn) ||
+        $('[data-cell="3"]').text() === playerTurn && $('[data-cell="4"]').text() === playerTurn && $('[data-cell="5"]').text() === playerTurn) ||
+        ($('[data-cell="6"]').text() === playerTurn && $('[data-cell="7"]').text() === playerTurn && $('[data-cell="8"]').text() === playerTurn);
+}
+​
+function verticalWin() {
+    return $('[data-cell="0"]').text() === playerTurn && ('[data-cell="3"]').text() === playerTurn && ('[data-cell="6"]').text() === playerTurn) ||
+        $('[data-cell="1"]').text() === playerTurn && $('[data-cell="4"]').text() === playerTurn && $('[data-cell="7"]').text() === playerTurn) ||
+        ($('[data-cell="2"]').text() === playerTurn && $('[data-cell="5"]').text() === playerTurn && $('[data-cell="8"]').text() === playerTurn);
+}
+​
+function diagonalWin() {
+    return ($('[data-cell="0"]').text() === playerTurn && $('[data-cell="4"]').text() === playerTurn && ('[data-cell="8"]').text() === playerTurn) ||
+        ($('[data-cell="6"]').text() === playerTurn && $('[data-cell="4"]').text() === playerTurn && $('[data-cell="2"]').text() === playerTurn);
 
-
+function checkForWin(){
+	if (horizontalWin() || verticalWin()|| diagonalWin()){
+		$('#announce-winner').text('player' + playerturn + 'won!')
+	}
+}
 
 
 }); 
