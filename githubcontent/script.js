c4a2bdb26411d075f290d517f20bb5bac626ee1d
reneedudley <4reneedudley@gmail.com>
2015-10-30 11:31:22 -0500

    initial shit

---
+++ githubcontent/script.js
@@ -0,0 +1,39 @@
+
+'use strict';
+
+$(document).ready(function(){
+	$.ajax('https://api.github.com/users/reneedudley/repos', {
+		success: function (response){
+			_.each(response, function(repos){
+				var str= '<li> <a class="repo" href=# data-repo="https://api.github.com/repos/reneedudley/'+repos['name']+'/contents">'+repos['name']+'</a></li>';
+				console.log(str);
+				$('#repos').append(str);
+			});
+  
+		}
+	});
+
+});
+
+// $(document).ready(function(){
+// 	$.ajax('https://reqres-api.herokuapp.com/api/users', {
+// 		success: function (response){
+// 			_.each(response, function(user){
+// 				var str = '<li> <a class="person" href=# data-user="https://reqres-api.herokuapp.com/api/users/'+user['id']+'"">' + user['first_name']+ ' '+ user['last_name']+'</a></li>';
+			
+// 				$('#people').append(str);
+// 			});
+
+// 			$('.person').on('click',function(event){
+// 				event.preventDefault();
+// 				$.ajax ( $(this).attr('data-user'), {
+// 					success: function(user){
+// 						var str = user['first_name']+ " " + user['last_name'];
+// 						str += '<br><img src="'+ user['avatar']+ '">'
+// 						$('#person-details').html(str);
+// 					}
+// 				});
+
+// 			});
+// 		}
+// 	});
