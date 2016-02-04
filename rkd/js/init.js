ec46cd41b098bbc82b844260adad6a598280d548
reneedudley <4reneedudley@gmail.com>
2016-02-04 13:58:52 -0600

    changed templates, js broke, great

--- rkd/js/init.js
+++ rkd/js/init.js
@@ -1,8 +1,20 @@
-(function($){
-  $(function(){
-
+$(function(){
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+    $('.carousel').carousel();
+    $('#arrow').pushpin({ top: $('#arrow').offset().top });
+    var divs = $('#arrow');
+    var header = $('#arrow');
+    $(window).scroll(function() {
+     header.addClass('lower-opacity');
+     if($(this).scrollTop() === 0) {
+        header.removeClass('lower-opacity');
+     }
+    });
+    var options = [
+    {selector: '#staggered-test', offset: 300, callback: 'Materialize.showStaggeredList("#staggered-test")'  },
+    {selector: '#staggered-test2', offset: 350, callback: 'Materialize.showStaggeredList("#staggered-test2")'  }
+    ];
+    Materialize.scrollFire(options);
 
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+}); // end of document ready// end of jQuery name space
