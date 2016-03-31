60878b0f2c37b7e38cc89aea17d6d5087bbfb489
Renee Dudley <reneedudley@Renees-MacBook-Pro.local>
2016-03-31 14:30:04 -0500

    projects section, fresco plugin

--- js/init.js
+++ js/init.js
@@ -13,8 +13,13 @@ $(function(){
    $('.profile').click(function(){
     $('#profile').removeClass('hide')
    });
-   
+   $('.zoom').hover(function(){
+    $(this).css("transform", "scale(1.3, 1.3)");
+    }, function(){
+    $(this).css("transform", "none");
+   });
 
+  
 
 
 }); // end of document ready// end of jQuery name space
