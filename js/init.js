462cd947e8321f14c671139d2212d4a66b316c0d
reneedudley <4reneedudley@gmail.com>
2016-04-04 19:38:31 -0500

    added jquery and pic

--- js/init.js
+++ js/init.js
@@ -1,4 +1,4 @@
-'use strict';
+
 
 $(function(){
     $('.button-collapse').sideNav();
@@ -10,16 +10,75 @@ $(function(){
     ];
     Materialize.scrollFire(options);
    
-   $('.profile').click(function(){
-    $('#profile').removeClass('hide')
-   });
+   
    $('.zoom').hover(function(){
     $(this).css("transform", "scale(1.3, 1.3)");
     }, function(){
     $(this).css("transform", "none");
    });
+   $('.one').click(function(){
+    $('#one').removeClass('leave');
+    $('#two').addClass('leave');
+    $('#three').addClass('leave');
+    $('#four').addClass('leave');
+    $('#five').addClass('leave');
+    $('#six').addClass('leave');
+    $('#seven').addClass('leave');
+   });
+   $('.two').click(function(){
+    $('#two').removeClass('leave');
+    $('#three').addClass('leave');
+    $('#four').addClass('leave');
+    $('#five').addClass('leave');
+    $('#six').addClass('leave');
+    $('#one').addClass('leave');
+     $('#seven').addClass('leave');
+   });
+   $('.three').click(function(){
+    $('#three').removeClass('leave');
+    $('#two').addClass('leave');
+    $('#four').addClass('leave');
+    $('#five').addClass('leave');
+    $('#six').addClass('leave');
+     $('#seven').addClass('leave');
+    $('#one').addClass('leave');
+   });
+   $('.four').click(function(){
+    $('#four').removeClass('leave');
+    $('#two').addClass('leave');
+    $('#five').addClass('leave');
+    $('#six').addClass('leave');
+    $('#one').addClass('leave');
+    $('#three').addClass('leave');
+     $('#seven').addClass('leave');
+   });
+   $('.five').click(function(){
+    $('#five').removeClass('leave');
+    $('#two').addClass('leave');
+    $('#six').addClass('leave');
+    $('#one').addClass('leave');
+    $('#three').addClass('leave');
+    $('#four').addClass('leave');
+     $('#seven').addClass('leave');
+   });
+   $('.six').click(function(){
+    $('#six').removeClass('leave');
+    $('#two').addClass('leave');
+    $('#five').addClass('leave');
+    $('#one').addClass('leave');
+    $('#three').addClass('leave');
+    $('#four').addClass('leave');
+     $('#seven').addClass('leave');
+   });
 
-  
-
+   $('.home').click(function(){
+    $('#seven').removeClass('leave');
+    $('#two').addClass('leave');
+    $('#five').addClass('leave');
+    $('#one').addClass('leave');
+    $('#three').addClass('leave');
+    $('#four').addClass('leave');
+     $('#six').addClass('leave');
+   });
 
 }); // end of document ready// end of jQuery name space
